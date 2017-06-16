daf428dcf71841620fb3c4c2587d3f4cf33ee646
neodevelop <juan@makingdevs.com>
2017-06-16 12:03:18 -0500

    Agregando la clase Project y la prueba

---
+++ grails-app/domain/com/makingdevs/Project.groovy
@@ -0,0 +1,15 @@
+package com.makingdevs
+
+class Project {
+
+  String owner
+  String codeName
+  String description
+  Boolean publicVisible
+
+  Date dateCreated
+  Date lastUpdated
+
+  static constraints = {
+  }
+}
