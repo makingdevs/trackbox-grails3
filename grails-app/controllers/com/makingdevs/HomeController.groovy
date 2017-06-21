7a391231f9443a22c3a756ddec4fefe0bcb6ac34
neodevelop <juan@makingdevs.com>
2017-06-21 12:31:44 -0500

    Diferencias entre modelos y commands

--- grails-app/controllers/com/makingdevs/HomeController.groovy
+++ grails-app/controllers/com/makingdevs/HomeController.groovy
@@ -8,12 +8,41 @@ class HomeController {
     [ projects : Project.list() ]
   }
 
-  def create(){
-    def project = new Project(params)
+  def create(Project project){
     log.debug "${project.validate()}"
     log.debug "${project.errors}"
+    log.debug "${project.properties}"
     project.save(flush: true)
     render project
   }
 
+  def validate(ProjectCommand projectCommand){
+    log.debug "${projectCommand.validate()}"
+    log.debug "${projectCommand.errors}"
+    log.debug "${projectCommand.properties}"
+    render projectCommand
+  }
+
+}
+
+class ProjectCommand {
+  String owner
+  String codeName
+  String description
+  Boolean publicVisible
+  List sprints = []
+  List backlog = []
+
+  static constraints = {
+    owner blank:false,size:1..100
+    codeName blank:false,size:1..50,unique:true
+    description blank:true, size:0..1000
+    publicVisible()
+    sprints minSize:1
+    backlog minSize:1
+  }
+
+  Project createProject(){
+    // ...
+  }
 }
