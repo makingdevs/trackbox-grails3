63e15d75be53e3e019a280492204b01e2265a4b0
neodevelop <juan@makingdevs.com>
2017-06-21 11:49:14 -0500

    Varios ejemplos

--- grails-app/controllers/com/makingdevs/HomeController.groovy
+++ grails-app/controllers/com/makingdevs/HomeController.groovy
@@ -4,6 +4,23 @@ class HomeController {
 
   def index() {
     log.debug "Hola mundo ${new Date()}"
+    log.debug "${params}"
+    params.each { k,v ->
+      log.debug "${v?.class}"
+    }
+    log.debug "${request}"
+    log.debug "${session.properties}"
     render "Hola mundo"
+  }
+
+  def accion1(){
+    flash.variable = "Estaré disponible en el siguiente request"
+    //redirect action:"accion2" // Generando un nuevo request
+    redirect controller: 'project', action: 'create'
+  }
+
+  def accion2(){
+    log.debug flash.variable // "Estaré disponible en el siguiente request"
+    render "Terminado"
   }
 }
