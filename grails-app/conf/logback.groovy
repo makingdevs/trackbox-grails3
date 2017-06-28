9844975cce31eefeddb3e3c68492e91107d06ab9
neodevelop <juan@makingdevs.com>
2017-06-28 12:54:41 -0500

    Ajustando log

--- grails-app/conf/logback.groovy
+++ grails-app/conf/logback.groovy
@@ -34,6 +34,8 @@ if (Environment.isDevelopmentMode() && targetDir != null) {
     logger("StackTrace", ERROR, ['FULL_STACKTRACE'], false)
 }
 
-logger 'grails.app.controllers', DEBUG, ['STDOUT'], false
-logger 'grails.app.services', DEBUG, ['STDOUT'], false
+//logger 'grails.app.controllers', DEBUG, ['STDOUT'], false
+//logger 'grails.app.services', DEBUG, ['STDOUT'], false
+//logger 'org.springframework.transaction', TRACE, ['STDOUT'], false
+logger 'org.grails.orm.hibernate.GrailsHibernateTransactionManager', DEBUG, ['STDOUT'], false
 root(ERROR, ['STDOUT'])
