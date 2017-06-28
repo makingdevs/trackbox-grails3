952b859602fd989159f8f9e68430b4573e4c6cc9
neodevelop <juan@makingdevs.com>
2017-06-28 10:20:05 -0500

    Agregando una prueba para el servicio

--- src/test/groovy/com/makingdevs/ProjectServiceSpec.groovy
+++ src/test/groovy/com/makingdevs/ProjectServiceSpec.groovy
@@ -1,22 +1,35 @@
 package com.makingdevs
 
 import grails.test.mixin.TestFor
+import grails.test.mixin.Mock
 import spock.lang.Specification
 
 /**
  * See the API for {@link grails.test.mixin.services.ServiceUnitTestMixin} for usage instructions
  */
 @TestFor(ProjectService)
+@Mock([Project])
 class ProjectServiceSpec extends Specification {
 
-    def setup() {
-    }
+  def setup() {
+  }
 
-    def cleanup() {
-    }
+  def cleanup() {
+  }
 
-    void "test something"() {
-        expect:"fix me"
-            true == false
-    }
+  void "starting a project"() {
+    given:
+    Project p = new Project(
+      owner:owner,
+      codeName:codeName,
+      description:description,
+      publicVisible:publicVisible)
+    when:
+      Project projectCreated = service.startNewProject(p)
+    then:
+      projectCreated.id
+    where:
+    owner        | codeName          | description     | publicVisible
+    "makingdevs" | "SECRETE_PROJECT" | "A description" | true
+  }
 }
