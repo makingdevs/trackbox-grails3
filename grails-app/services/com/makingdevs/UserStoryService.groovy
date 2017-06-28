76ae0848935bb9296b3d02d09b8294ca6f38a2fc
neodevelop <juan@makingdevs.com>
2017-06-28 12:55:58 -0500

    Agregando tipo de propagacion

--- grails-app/services/com/makingdevs/UserStoryService.groovy
+++ grails-app/services/com/makingdevs/UserStoryService.groovy
@@ -1,10 +1,12 @@
 package com.makingdevs
 
 import grails.transaction.Transactional
+import org.springframework.transaction.annotation.Propagation
 
 @Transactional
 class UserStoryService {
 
+  @Transactional(propagation = Propagation.REQUIRES_NEW)
   def serviceMethod() {
     log.debug "Tocando User Story Service"
   }
